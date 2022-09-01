2253477d1c946bb3905a1f45bcdd47be38c72a89
DeniOS777 <Denios777@gmail.com>
2022-09-01 19:44:55 +0300

    rewrited Searchbar to hooks

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,62 +1,57 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import { toast } from 'react-toastify';
 import PropTypes from 'prop-types';
 import { IoSearchOutline } from 'react-icons/io5';
-
 import { Box } from '../Box';
 import { Form, Input, Button } from './Searchbar.styled';
 
-export class Searchbar extends Component {
-  static defaultProps = {
-    onSubmit: PropTypes.func.isRequired,
-  };
+const Searchbar = ({ onSubmit }) => {
+  const [searchQuery, setSearchQuery] = useState('');
 
-  state = {
-    searchQuery: '',
-  };
+  const handleChange = e => setSearchQuery(e.target.value);
 
-  handleChange = e => this.setState({ searchQuery: e.target.value });
-
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
 
-    if (this.state.searchQuery.trim() === '') {
-      return this.emptySearchFieldNotification();
+    if (searchQuery.trim() === '') {
+      return emptySearchFieldNotification();
     }
-    this.props.onSubmit(this.state.searchQuery);
+    onSubmit(searchQuery);
   };
 
-  emptySearchFieldNotification = () => {
+  const emptySearchFieldNotification = () => {
     toast.info('Please enter a keyword for search');
   };
 
-  render() {
-    const { searchQuery } = this.state;
+  return (
+    <Box
+      as="header"
+      position="fixed"
+      display="flex"
+      justifyContent="center"
+      p={2}
+      width="100%"
+      bg="blue"
+    >
+      <Form onSubmit={handleSubmit}>
+        <Input
+          onChange={handleChange}
+          type="text"
+          value={searchQuery}
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+        <Button type="submit" aria-label="search button">
+          <IoSearchOutline />
+        </Button>
+      </Form>
+    </Box>
+  );
+};
 
-    return (
-      <Box
-        as="header"
-        position="fixed"
-        display="flex"
-        justifyContent="center"
-        p={2}
-        width="100%"
-        bg="blue"
-      >
-        <Form onSubmit={this.handleSubmit}>
-          <Input
-            onChange={this.handleChange}
-            type="text"
-            value={searchQuery}
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-          />
-          <Button type="submit" aria-label="search button">
-            <IoSearchOutline />
-          </Button>
-        </Form>
-      </Box>
-    );
-  }
-}
+Searchbar.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
+
+export { Searchbar };
