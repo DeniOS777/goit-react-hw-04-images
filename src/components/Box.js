bac37cd6914f3e0d9d364410643b1416238d6a33
DeniOS777 <Denios777@gmail.com>
2022-09-02 18:07:00 +0300

    update styles of Searchbar

--- src/components/Box.js
+++ src/components/Box.js
@@ -7,6 +7,7 @@ import {
   grid,
   border,
   position,
+  shadow,
 } from 'styled-system';
 
 export const Box = styled.div`
@@ -17,4 +18,5 @@ export const Box = styled.div`
   ${grid}
   ${border}
   ${position}
+  ${shadow}
 `;
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -32,6 +32,7 @@ const Searchbar = ({ onSubmit }) => {
       p={2}
       width="100%"
       bg="blue"
+      boxShadow="0px 10px 13px -7px #000000, 5px 5px 15px 5px rgba(0,0,0,0)"
     >
       <Form onSubmit={handleSubmit}>
         <Input
