798ae74a5df5e1a7dd04d2a2ef7b7ed188899141
DeniOS777 <Denios777@gmail.com>
2022-09-02 17:19:28 +0300

    change request with searchParams

--- src/services/api.js
+++ src/services/api.js
@@ -4,9 +4,15 @@ const API_KEY = '25243201-da43b78e8715fb1cc3094e420';
 
 axios.defaults.baseURL = 'https://pixabay.com/api';
 
+const searchParams = new URLSearchParams({
+  key: API_KEY,
+  image_type: 'photo',
+  orientation: 'horizontal',
+});
+
 export const getImages = async (query, page, perPage) => {
   const response = await axios.get(
-    `/?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${perPage}`
+    `/?q=${query}&page=${page}&per_page=${perPage}&${searchParams}`
   );
   return response.data;
 };
